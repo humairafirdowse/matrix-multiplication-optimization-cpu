f3e90e183ab98c0b6718d151cdce803c092b9b8a
hfmohammed18 <hfmohammed18@ucsd.edu>
2024-10-25 02:38:48 +0000

    version2-sve

--- blislab/bl_dgemm_ukr.c
+++ blislab/bl_dgemm_ukr.c
@@ -17,25 +17,23 @@ void bl_dgemm_ukr( int    k,
                    unsigned long long ldc,
                    aux_t* data )
 {
-    int l, j, i;
-
-    for ( l = 0; l < k; ++l )
-    {                 
-        for ( j = 0; j < n; ++j )
-        { 
-            for ( i = 0; i < m; ++i )
-            { 
-	      // ldc is used here because a[] and b[] are not packed by the
-	      // starter code
-	      // cse260 - you can modify the leading indice to DGEMM_NR and DGEMM_MR as appropriate
-	      //
-	      c( i, j, ldc ) += a( l, i, m) * b( l, j, n );  //A is in column major order and B is in row major ordering  
-            }
+    register svfloat64_t ax;
+    register svfloat64_t bx;
+    register svfloat64_t c0x;
+    svbool_t npred = svwhilelt_b64_u64(0, n);
+    
+    for (int kk=0; kk<k; kk++){
+        for(int i=0;i<m;i++){
+            register float64_t aval = *(a + kk*m + i);
+            c0x = svld1_f64(npred, c+i*ldc);
+            ax =svdup_f64(aval);
+            bx = svld1_f64(svptrue_b64(), b + kk*n);
+            c0x =svmla_f64_m(npred, c0x, bx, ax);
+            svst1_f64(npred, c+i*ldc, c0x);
         }
+
     }
-
 }
-
 
 // cse260
 // you can put your optimized kernels here
